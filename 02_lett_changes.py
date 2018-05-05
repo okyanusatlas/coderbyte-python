0cadecffd0895993aec0876be0a2e6166eb6b0bc
Okyanus Atlas <okyanus.atlas@outlook.de>
2018-05-05 08:44:47 +0200

    Add files via upload

---
+++ 02_lett_changes.py
@@ -0,0 +1,30 @@
+"""
+Using the Python language, have the function LetterChanges(str) take the str parameter being passed and modify it using
+the following algorithm. Replace every letter in the string with the letter following it in the alphabet
+(ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this
+modified string.
+"""
+
+def LetterChanges(str):
+    str = list(str)
+    asciiList = []
+    for letter in str:
+        asciiList.append(ord(letter))
+
+    for counter, ascii_code in enumerate(asciiList):
+        if ascii_code > 64:
+            ascii_code += 1
+            if chr(ascii_code) == "a" or chr(ascii_code) == "e" or chr(ascii_code) == "i" or chr(ascii_code) == "o" or chr(ascii_code) == "u":
+                asciiList[counter] = chr(ascii_code).capitalize()
+            else:
+                asciiList[counter] = chr(ascii_code)
+        else:
+            asciiList[counter] = chr(ascii_code)
+
+
+    result = ''.join(asciiList)
+
+    return result
+
+
+print(LetterChanges("beautiful^"))
