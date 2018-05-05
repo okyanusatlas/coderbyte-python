4e1ff097f55abeeeb686acd93726a3ec6e3df843
Okyanus Atlas <okyanus.atlas@outlook.de>
2018-05-05 10:42:37 +0200

    Add files via upload

---
+++ 05_simple_adding.py
@@ -0,0 +1,15 @@
+"""
+Using the Python language, have the function SimpleAdding(num) add up all the numbers from 1 to num. For example: if the
+ input is 4 then your program should return 10 because 1 + 2 + 3 + 4 = 10. For the test cases, the parameter num will be
+ any number from 1 to 1000.
+"""
+def SimpleAdding(num):
+
+    if num <= 1:
+        return 1
+    else:
+        return num + SimpleAdding(num-1)
+
+
+
+print(SimpleAdding(140))
